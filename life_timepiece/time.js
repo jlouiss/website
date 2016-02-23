bbf6e5f7df7b4d85660962d9c2c54c6d98765f7c
jlouiss <jlsalbego@gmail.com>
2016-02-23 21:39:19 +0100

    added 2 digit function

--- life_timepiece/time.js
+++ life_timepiece/time.js
@@ -6,6 +6,11 @@ $(document).ready(function () {
         centsPerDay = sPerDay * 100,
         msPerDay = sPerDay * 1000,
         birthDate = new Date('July 18, 1994');
+    
+    // 2 digit numbers
+    function pad2(number) {
+        return (number < 10 ? '0' : '') + number;
+    }
 
     // calculates seconds to next day
     setInterval(function () {
@@ -13,18 +18,19 @@ $(document).ready(function () {
             centsToday = Math.floor(((now.getTime()) % msPerDay) / 10),
             seconds = Math.floor((centsPerDay - centsToday - timeDifference / 10) / 100);
         document.getElementById('secondsLeft').innerHTML = seconds;
-        document.getElementById('cents').innerHTML = Math.floor(now.getMilliseconds() / 10);
+        document.getElementById('cents').innerHTML = pad2(Math.floor(now.getMilliseconds() / 10));
         // document.getElementById('test').innerHTML = now;
     }, interval);
 
     // calculates age in days
     var msSince = (now.getTime() - birthDate.getTime() + timeDifference),
-        days = Math.floor(msSince / msPerDay);
+        days = Math.floor(msSince / msPerDay) + 1;
     document.getElementById('ageInDays').innerHTML = days;
 
     // calculates days to first day of next year
-    var firstOfYear = new Date('January 1, ' + (now.getFullYear() + 1)),
+    var nextYear = now.getFullYear() + 1,
+        firstOfYear = new Date('January 1, ' + nextYear),
         msLeft = (firstOfYear.getTime() - now.getTime() - timeDifference),
-        daysLeft = Math.floor(msLeft / msPerDay);
+        daysLeft = Math.floor(msLeft / msPerDay) + 1;
     document.getElementById('daysLeft').innerHTML = daysLeft;
 });
