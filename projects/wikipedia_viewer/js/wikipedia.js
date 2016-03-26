4f9aeeef7444010dd02fd89fbe5d9a60399d1eec
jlouiss <jlsalbego@gmail.com>
2016-03-26 01:32:26 +0100

    completed wikipedia viewer

--- projects/wikipedia_viewer/js/wikipedia.js
+++ projects/wikipedia_viewer/js/wikipedia.js
@@ -1,17 +1,50 @@
 $(document).ready(function() {
+  function getCrit() {
+    var criteria = $('#search input').val();
+    console.log(criteria);
 
-    $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=Brazil',
+    if (criteria.length > 0) {
+      $('#title').html('I want to know about ' + criteria);
+      return criteria;
+    } else {
+      $('#wrapper').removeClass('active');
+      $('#title').html('I want to know about...');
+      $('#results').hide();
+    }
+  };
+
+  function search() {
+    var criteria = getCrit();
+
+    if (criteria) {
+      $('#results').show();
+      $('#wrapper').addClass('active');
+
+      $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + criteria,
         dataType: 'jsonp',
         headers: { 'Api-User-Agent': 'Example/1.0' },
         success: function(data) {
-            console.log(data);
-            var data = data.query.search;
-            console.log(data);
-            for (i in data) {
-                $('.results').append('<h1 class="text-left">' + data[i].title +
-                    '</h1><p class="text-right">' + data[i].snippet + '</p>');
-            }
+          console.log(data);
+          var data = data.query.search;
+          console.log(data);
+          for (i in data) {
+            $('#results').append(
+              '<a href="' + 'https://en.wikipedia.org/wiki/' + data[i].title +
+              '" target="_blank"' + '"><h1 class="text-left">' + data[i].title +
+              '</h1><p class="text-right">' + data[i].snippet + '</p></a>');
+          }
         }
-    });
+      }); // ajax
+    } // if
+  };
+
+  $('#search i').on('click', function() {
+    search();
+  });
+  $('input').keypress(function(e) {
+    if (e.which == 13) search();
+  });
+
+  $('#results').hide();
 });
