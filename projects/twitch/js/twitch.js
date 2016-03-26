a51440afa710ec3be947a03e874740fc61793b95
jlouiss <jlsalbego@gmail.com>
2016-03-26 19:00:37 +0100

    started twitchtv project

---
+++ projects/twitch/js/twitch.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var streamers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx",
+    "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff"];
+  var requestURL = 'https://api.twitch.tv/kraken/streams/';
+
+  for (var i in streamers) {
+    var request = requestURL + streamers[i] + '?callback=?';
+    retrieveData(request, streamers[i]);
+  }
+
+  function retrieveData(request, channel) {
+    $.getJSON(request,
+      function(data) {
+        console.log(channel);
+        console.log(data);
+        console.log('\n\n\n');
+      });
+  }
+});
