684eb9ec298feed3d5eeba04f2704b19eabe2b33
jlouiss <jlsalbego@gmail.com>
2016-03-30 19:26:49 +0200

    fixed pomodoro timer

--- projects/pomodoro_timer/js/timer.js
+++ projects/pomodoro_timer/js/timer.js
@@ -1,104 +1,103 @@
-$(document).ready(function() {
-    var pomodoro = 25,
-        pause = 5,
-        time = 0,
-        mins = 0,
-        secs = 0,
-        interval = 100, // ms
-        select = false, // false = pomodoro, true = break
-        timer,
-        check = false,
-        alarm = document.createElement('audio');
+var pomodoro = 25,
+    pause = 5,
+    time = 0,
+    mins = 0,
+    secs = 0,
+    timer,
+    interval = 100, // ms
+    select = false, // false = pomodoro, true = break
+    check = false,
+    alarm = document.createElement('audio');
 
-    alarm.setAttribute('src', 'http://www.soundjay.com/button/beep-07.mp3');
+alarm.setAttribute('src', 'http://www.soundjay.com/button/beep-07.mp3');
 
-    // return 2 digit number
-    function pad2(number) {
-        return (number < 10 ? '0' : '') + number;
+// return 2 digit number
+var pad2 = function(number) {
+    return (number < 10 ? '0' : '') + number;
+};
+
+// resets pomodoro to 25 and pause to 5
+var reset = function() {
+    clearInterval(timer);
+
+    time = pomodoro * 60;
+    check = false;
+    select = false;
+
+    mins = pad2(Math.floor(time / 60));
+    secs = pad2(Math.floor(time % 60));
+
+    $('#time').html(mins + ':' + secs);
+};
+
+// core function
+var watch = function() {
+    if (!check) {
+        check = true;
+    } else {
+        check = false;
+        clearInterval(timer);
     }
 
-    //display initial time
-    $('#time').html('Start');
-    $('#b-time').html(pause);
-    $('#p-time').html(pomodoro);
-
-    // set timers
-    $('#b-minus').click(function() {
-        if (!check && pause > 1) {
-            pause -= 1;
-            $('#b-time').html(pause);
-        }
-    });
-
-    $('#b-plus').click(function() {
-        if (!check) {
-            pause += 1;
-            $('#b-time').html(pause);
-        }
-    });
-
-    $('#p-minus').click(function() {
-        if (!check && pomodoro > 1) {
-            pomodoro -= 1;
-            $('#p-time').html(pomodoro);
-        }
-    });
-
-    $('#p-plus').click(function() {
-        if (!check) {
-            pomodoro += 1;
-            $('#p-time').html(pomodoro);
-        }
-    });
-
-    $('#reset').click(function() {
-        clearInterval(timer);
-
+    if ($('#time').html() == 'Start')
         time = pomodoro * 60;
-        check = false;
-        select = false;
 
-        mins = pad2(Math.floor(time / 60));
-        secs = pad2(Math.floor(time % 60));
+    if (check) {
+        timer = setInterval(function() {
+            time -= interval / 1000; // 100 ms
+            mins = pad2(Math.floor(time / 60));
+            secs = pad2(Math.floor(time % 60));
+            $('#time').html(mins + ':' + secs);
 
-        $('#time').html(mins + ':' + secs);
-    });
-
-    $('#circle').click(function() {
-        if (!select)
-            time = pomodoro * 60;
-        else
-            time = pause * 60;
-
-        if (!check) {
-            check = true;
-        } else {
-            check = false;
-            clearInterval(timer);
-        }
-
-        if (check) {
-            timer = setInterval(function() {
-
-                time -= interval / 1000; // 100 ms
-                mins = pad2(Math.floor(time / 60));
-                secs = pad2(Math.floor(time % 60));
-                $('#time').html(mins + ':' + secs);
-
-                if (time <= 0) {
-                    $('#time').html('00:00');
-                    alarm.play();
-                    if (select) {
-                        time = pomodoro * 60;
-                        select = false;
-                    } else {
-                        time = pause * 60;
-                        select = true;
-                    }
+            if (time <= 0) {
+                $('#time').html('00:00');
+                alarm.play();
+                if (select) {
+                    time = pomodoro * 60;
+                    select = false;
+                } else {
+                    time = pause * 60;
+                    select = true;
                 }
+            }
 
-            }, interval);
-        }
+        }, interval);
+    }
+};
 
-    });
+$('#reset').click(reset);
+$('#circle').click(watch);
+
+//display initial time
+$('#time').html('Start');
+$('#b-time').html(pause);
+$('#p-time').html(pomodoro);
+
+// set timers
+$('#b-minus').click(function() {
+    if (!check && pause > 1) {
+        pause--;
+        $('#b-time').html(pause);
+    }
+});
+
+$('#b-plus').click(function() {
+    if (!check) {
+        pause++;
+        $('#b-time').html(pause);
+    }
+});
+
+$('#p-minus').click(function() {
+    if (!check && pomodoro > 1) {
+        pomodoro--;
+        $('#p-time').html(pomodoro);
+    }
+});
+
+$('#p-plus').click(function() {
+    if (!check) {
+        pomodoro++;
+        $('#p-time').html(pomodoro);
+    }
 });
