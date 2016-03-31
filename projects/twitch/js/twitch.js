3fffea5c055881758036cb42689efb39c4aa0083
jlouiss <jlsalbego@gmail.com>
2016-03-31 23:18:56 +0200

    completed twitch viewer

--- projects/twitch/js/twitch.js
+++ projects/twitch/js/twitch.js
@@ -1,43 +1,80 @@
-// bootsrap tabs
-$('#myTabs a').click(function(e) {
-    e.preventDefault()
-    $(this).tab('show')
-});
+// some streamers + some from socialblade.com's top 100 twitch streamers (most viewed)
+var streamers = [
+    'freecodecamp', 'storbeck', 'terakilobyte', 'beohoff', 'RobotCaleb',
+    'thomasballinger', 'noobs2ninjas', 'habathcx', 'riotgames', 'starladder1',
+    'beyondthesummit', 'tsm_theoddone', 'Tsm_dyrus', 'esl_csgo', 'garenatw',
+    'HiRezTV', 'smitegame', 'Nightblue3', 'nl_kripp', 'imaqtpie', 'esl_lol',
+    'asiagodtonegg3be0', 'destructoid', 'sodapoppin', 'OGNGlobal', 'ongamenet',
+    'joindotared', 'faceittv', 'taketv', 'versuta', 'PhantomL0rd', 'Voyboy',
+    'wingsofdeath', 'towelliee', 'TrumpSC', 'leveluplive', 'twitch', 'itshafu',
+    'dotastarladder_en', 'riotgamesturkish', 'twitchplayspokemon', 'kingkongor',
+    'aces_tv', 'gamespot', 'sc2proleague', 'SirhcEz', 'totalbiscuit', 'mlgsc2',
+    'scarra', 'RocketBeansTV', 'lethalfrag', 'dendi', 'wcs_america', 'mlglol',
+    'defrancogames', 'shadbasemurdertv', 'yogscast', 'Imt_wildturtle', 'magic',
+    'streamerhouse', 'dhingameclient', 'wcs_europe', 'sing_sing', 'roomonfire',
+    'onemoregametv', 'dreamleague', 'syndicate', 'saintvicious',
+    'brunofin', 'comster404'
+];
 
-$(document).ready(function() {
-    function retrieveData(request, channel) {
-        $.getJSON(request, function(data) {
-            console.log(channel);
-            console.log(data);
-            console.log('\n\n\n');
+var app = angular.module('app', []);
 
-            if (data.stream)
-                $('#online').append('<h1>' + channel + '</h1>');
-            else
-                $('#offline').append('<h1>' + channel + '</h1>');
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+    var requestURL = 'https://api.twitch.tv/kraken/streams/',
+        search = '';
 
-            $('#all').append('<h1>' + channel + '</h1>');
-        });
-    }
+    $scope.streamers = [];
 
-    var streamers = [
-    "freecodecamp",
-    "storbeck",
-    "terakilobyte",
-    "habathcx",
-    "RobotCaleb",
-    "thomasballinger",
-    "noobs2ninjas",
-    "beohoff",
-    // should be online
-    "Skipnh0",
-    "hastad",
-    "RageFu",
-    "NWGamesBrasil"];
-    var requestURL = 'https://api.twitch.tv/kraken/streams/';
+    function makeChannel(name, data) {
+        return {
+            'name': name,
+            'online': data.stream != null,
+            'data': data.stream,
+            'exists': true
+        };
+    };
+
+    function fetch(request, channel) {
+        $http.get(request)
+            .then(function(response) {
+                if (channel == 'freecodecamp')
+                    $scope.streamers.unshift(makeChannel(channel, response.data));
+                else
+                    $scope.streamers.push(makeChannel(channel, response.data));
+
+                console.log(channel);
+                console.log(makeChannel(channel, response.data));
+                console.log(response.data.stream);
+
+                console.log('\n\nStreamers:\n');
+                console.log($scope.streamers);
+            }, function() {
+                console.log('Can\'t fetch streamer ' + channel);
+                $scope.streamers.unshift({
+                    'name': channel,
+                    'exists': false
+                });
+            });
+    };
 
     for (var i in streamers) {
-        var request = requestURL + streamers[i] + '?callback=?';
-        retrieveData(request, streamers[i]);
-    }
-});
+        var request = requestURL + streamers[i];
+        console.log(request);
+        fetch(request, streamers[i]);
+
+        console.log('i:' + i);
+        console.log($scope.streamers);
+        console.log('\n\n');
+    };
+}]);
+
+app.controller('TabController', ['$scope', function($scope) {
+    $scope.tab = 'online';
+
+    $scope.setTab = function(tab) {
+        $scope.tab = tab;
+    };
+
+    $scope.isSet = function(tab) {
+        return $scope.tab == tab;
+    };
+}]);
