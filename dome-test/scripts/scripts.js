17706788544b5f8add2beed001f84d83e23d1cef
jlouiss <jlsalbego@gmail.com>
2016-10-17 22:13:06 +0200

    trying hard

--- dome-test/scripts/scripts.js
+++ dome-test/scripts/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function smoothScroll() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -10,5 +10,18 @@ $(document).ready(function() {
         return false;
       }
     }
+  });
+
+  var team = {
+    luca: 'http://www.domedesign.it/luca-rossi.html',
+    jean: 'http://www.domedesign.it/jean-louis-salbego.html',
+    orca: 'http://www.domedesign.it/laerte-schiavo.html',
+    youssef: 'http://www.domedesign.it/youssef-dalima.html',
+    muri: 'http://www.domedesign.it/nicola-murianni.html',
+    elisa: 'http://www.domedesign.it/elisa-benetti.html'
+  };
+
+  $('path').on('click', function() {
+    window.location = team[this.id.split('-')[0]];
   });
 });
