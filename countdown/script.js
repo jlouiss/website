ecd8681ee749080d21edd46db17231cbf52e3af5
Jean Louis Salbego <jlsalbego@gmail.com>
2016-12-05 15:42:47 +0100

    es5

--- countdown/script.js
+++ countdown/script.js
@@ -1,6 +1,6 @@
-let today = new Date();
-let endDate = new Date(Date.UTC(2016, 11, 30, 3, 0, 0));
-let dayInMs = 24 * 60 * 60 * 1000;
-let daysToEnd = (endDate - today) / dayInMs;
+var today = new Date();
+var endDate = new Date(Date.UTC(2016, 11, 31, 3, 0, 0));
+var dayInMs = 24 * 60 * 60 * 1000;
+var daysToEnd = (endDate - today) / dayInMs;
 
 document.getElementsByTagName('h1')[0].innerHTML = Math.floor(daysToEnd);
