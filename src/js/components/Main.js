04b58a567aee36fad0db7d00e5f16e2f6085dadd
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-10-03 08:40:35 +0200

    placeholder. please be patient, i will finish refactoring tonight

---
+++ src/js/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class Main extends React.Component {
+
+    render() {
+
+        return (
+            <header className="App-header">
+                <h1>Jean Louis Salbego</h1>
+                <h3>Full Stack Developer</h3>
+                <p>I am currently rewriting this site. Please be patient.</p>
+            </header>
+        );
+
+    }
+
+}
