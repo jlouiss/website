1ac1458138cf76c013b7499208415bffd5547fb4
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-10-03 22:36:23 +0200

    added basic styling

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -6,8 +6,8 @@ export default class Main extends React.Component {
 
         return (
             <header className="App-header">
-                <h1>Jean Louis Salbego</h1>
-                <h3>Full Stack Developer</h3>
+                <h1 className="title">Jean Louis Salbego</h1>
+                <h3 className="subtitle">Full Stack Developer</h3>
                 <p>I am currently rewriting this site. Please be patient.</p>
             </header>
         );
