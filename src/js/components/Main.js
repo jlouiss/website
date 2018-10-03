8dd40bf53fb9fbe7b75f6312e57c0859e92a4140
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-10-04 00:20:30 +0200

    added rel to anchors

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -11,28 +11,28 @@ export default class Main extends React.Component {
                     <h3 className="subtitle">Full Stack Developer</h3>
                 </header>
                 <p className="icons">
-                    <a href="https://github.com/jlouiss/jlouiss.github.io" target="_blank">
+                    <a href="https://github.com/jlouiss/jlouiss.github.io" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-github fa-2x"/>
                     </a>
-                    <a href="https://www.linkedin.com/in/jeanlouissalbego" target="_blank">
+                    <a href="https://www.linkedin.com/in/jeanlouissalbego" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-linkedin fa-2x"/>
                     </a>
-                    <a href="https://twitter.com/jlsalbego" target="_blank">
+                    <a href="https://twitter.com/jlsalbego" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-twitter fa-2x"/>
                     </a>
-                    <a href="https://codepen.io/JLouisS" target="_blank">
+                    <a href="https://codepen.io/JLouisS" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-codepen fa-2x"/>
                     </a>
-                    <a href="https://www.freecodecamp.org/jlouiss" target="_blank">
+                    <a href="https://www.freecodecamp.org/jlouiss" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-free-code-camp fa-2x"/>
                     </a>
-                    <a href="http://instagram.com/jeanlouissalbego" target="_blank">
+                    <a href="http://instagram.com/jeanlouissalbego" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-instagram fa-2x"/>
                     </a>
                 </p>
                 <small className="footer">
                     Please, follow the github link to find more about me.
-                    {/* TRUST YOUR GUTS, HIRE ME */}
+                    {/* HIRE ME!!! */}
                 </small>
             </React.Fragment>
         );
