6a971d4fe0fceed99ab1329fa36f78958530d652
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-10-03 21:29:14 +0200

    this is frustrating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import Main from './js/components/Main';
@@ -10,9 +9,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <BrowserRouter basename={process.env.PUBLIC_URL}>
-                    <Route exact path="/" component={Main}/>
-                </BrowserRouter>
+                <Main/>
             </div>
         );
     }
