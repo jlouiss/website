0111cecb6ea1ffca59faed1ff7a84677891713d4
Jean <info@jlouis.me>
2022-02-21 22:33:13 +0000

    Animation v2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import './App.scss';
 function App() {
   return (
     <div className="container d-flex justify-content-center align-items-center">
-      <div className="row mb-5">
+      <div className="row">
         <div className="col text-center">
           <h1 className="title">
-            <span className="hi">Hi, I'm </span><span className="name">Jean</span>
+            <span className="hi">Hi,{'\u00A0'}</span><span className="im">I'm{'\u00A0'}</span><span className="jean">Jean.</span>
           </h1>
-          <p className="subtitle">Nice to meet you</p>
-        </div>
-        <div className="row">
-
+          <p className="subtitle">Nice to meet you. :)</p>
         </div>
       </div>
     </div>
