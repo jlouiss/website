524594e0d0f6c91cee3167fb442c55e32f034909
Jean <info@jlouis.me>
2022-02-21 21:52:33 +0000

    Animation v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="row mb-5">
+        <div className="col text-center">
+          <h1 className="title">
+            <span className="hi">Hi, I'm </span><span className="name">Jean</span>
+          </h1>
+          <p className="subtitle">Nice to meet you</p>
+        </div>
+        <div className="row">
+
+        </div>
+      </div>
     </div>
   );
 }
