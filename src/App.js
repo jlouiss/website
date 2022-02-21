83d226b7ecabdb297a390d41ef49cd32041c122e
Jean <info@jlouis.me>
2022-02-21 22:39:06 +0000

    Animation v3 + link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           <h1 className="title">
             <span className="hi">Hi,{'\u00A0'}</span><span className="im">I'm{'\u00A0'}</span><span className="jean">Jean.</span>
           </h1>
-          <p className="subtitle">Nice to meet you. :)</p>
+          <p className="subtitle"><a href="https://github.com/jlouiss/website" rel="noreferrer">Nice to meet you. :)</a></p>
         </div>
       </div>
     </div>
